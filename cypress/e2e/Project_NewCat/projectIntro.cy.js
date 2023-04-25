59effdb4935090dab4ecf1bf8d6b9726d95e2fab
PeaceGusenga <gusengapeace@gmail.com>
2023-04-25 14:26:41 +0200

    query an element

--- cypress/e2e/Project_NewCat/projectIntro.cy.js
+++ cypress/e2e/Project_NewCat/projectIntro.cy.js
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('My First Test', () => {
+  it('finds the content "type"', () => {
     cy.visit('https://example.cypress.io')
+
+    cy.contains('type')
   })
 })
