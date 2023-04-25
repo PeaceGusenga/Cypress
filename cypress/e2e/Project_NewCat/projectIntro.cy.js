5a319054ed50c31f6e5f005dc2e1c95b59b9dc6b
PeaceGusenga <gusengapeace@gmail.com>
2023-04-25 14:36:49 +0200

    Extra Assertions

--- cypress/e2e/Project_NewCat/projectIntro.cy.js
+++ cypress/e2e/Project_NewCat/projectIntro.cy.js
@@ -1,10 +1,17 @@
 describe('My First Test', () => {
-  it('clicking "type" navigates to a new url', () => {
+  it('Gets, types and asserts', () => {
     cy.visit('https://example.cypress.io')
 
     cy.contains('type').click()
 
-    // Assertion to check URL is correct
-    cy.url().should('include','/commands/actions')
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it
+    cy.get('.action-email').type('fake@email.com')
+
+    //  Verify that the value has been updated
+    cy.get('.action-email').should('have.value', 'fake@email.com')
   })
 })
