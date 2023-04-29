1b42954edb3fe2367f2c006919af649798e63e5c
PeaceGusenga <gusengapeace@gmail.com>
2023-04-29 14:26:23 +0200

    added timeout

--- cypress/e2e/Project_NewCat/test1.cy.js
+++ cypress/e2e/Project_NewCat/test1.cy.js
@@ -5,14 +5,14 @@ it ('Google Search', function(){
     cy.visit('https://google.com/')
 
     //Find searchbar and enter text
-    cy.get('.gLFyf ').type('FC Bayern Munich {Enter}')
+    cy.get('.gLFyf ').type('FC Bayern Munich{Enter}')
 
     //Alternate way instead of {Enter}
     //click search button
     //cy.contains('Google Search').click()
 
     //click to display full fixtures
-    cy.get('.U0faLd > .PUDfGe > .z1asCe > svg').click()
+    cy.get('.U0faLd > .PUDfGe > .z1asCe > svg',{timeout:6000}).click()
 
     // click to display latest News
     cy.get('[tabindex="-1"] > .tb_u > .tb_h > .tb_sh > .PPjCfd > [data-tab_type="NEWS"]').click()
