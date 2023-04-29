6c844175e8d7387fc90d9d263b40e0012c35fb41
PeaceGusenga <gusengapeace@gmail.com>
2023-04-29 19:27:15 +0200

    Store Login Page Elements into Variables

--- cypress/e2e/Project_EagleEye/PageObjectModel/pages/login_page.js
+++ cypress/e2e/Project_EagleEye/PageObjectModel/pages/login_page.js
@@ -1,14 +1,18 @@
 export class LoginPage {
 
+    loginpage_textbox_username = ':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    loginpage_textbox_password = ':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input'
+    loginpage_loginbutton = '.oxd-button'
+
     enterUsername(username){
-        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+        cy.get(loginpage_textbox_username).type(username)
     }
 
     enterPassword(password){
-        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
+        cy.get(loginpage_textbox_password).type(password)
     }
 
     clickLogin(){
-        cy.get('.oxd-button').click()
+        cy.get(loginpage_loginbutton).click()
     }
 }
