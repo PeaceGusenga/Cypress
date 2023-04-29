1b6f98091fbf14cbc7581cea1e7913b80a9b59ad
PeaceGusenga <gusengapeace@gmail.com>
2023-04-29 19:14:40 +0200

    Login Page Object Model

--- cypress/e2e/Project_EagleEye/PageObjectModel/pomTest.cy.js
+++ cypress/e2e/Project_EagleEye/PageObjectModel/pomTest.cy.js
@@ -1,15 +1,14 @@
 import { LoginPage } from "./pages/login_page"
 
-const loginPage = new LoginPage() 
+var loginPage = new LoginPage() 
 
 it('Page Object Model Example', () => {
-    
     //Navigate to Demo login webpage
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     //Enter Username
-    loginPage.enterUsername
+    loginPage.enterUsername('Admin')
     //Enter Password
-    loginPage.enterPassword
+    loginPage.enterPassword('admin123')
     //Click login Button
-    loginPage.clickLogin
+    loginPage.clickLogin()
 })
