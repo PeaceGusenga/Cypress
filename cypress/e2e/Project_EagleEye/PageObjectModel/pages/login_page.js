665f62d43de5d4872675aaa7b501ff89f36b4af3
PeaceGusenga <gusengapeace@gmail.com>
2023-04-29 18:50:47 +0200

    Login Page Class

---
+++ cypress/e2e/Project_EagleEye/PageObjectModel/pages/login_page.js
@@ -0,0 +1,14 @@
+export class LoginPage {
+
+    enterUsername(){
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    }
+
+    enterPassword(){
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    }
+
+    clickLogin(){
+        cy.get('.oxd-button').click()
+    }
+}
