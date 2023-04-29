7c2835347bf1aac18f3c26882998b315ce3ff92c
PeaceGusenga <gusengapeace@gmail.com>
2023-04-29 14:34:35 +0200

    default timeout amendments

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,8 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
+    
+    defaultCommandTimeout: 5500,
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
