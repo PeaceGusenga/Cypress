d9229044059c23ca6459589a3cbe16b12fc07f27
PeaceGusenga <gusengapeace@gmail.com>
2023-05-01 15:56:11 +0200

    fix test methods

--- cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/homepage_link_Test.cy.js
+++ cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/homepage_link_Test.cy.js
@@ -7,82 +7,82 @@ describe('Test all the links on the landing page', () => {
         cy.visit('http://www.uitestingplayground.com/')
     })
     it('Test 1', () => {
-        landingPage.clickDynamicID_Page
+        landingPage.clickDynamicID_Page()
     })
 
     it('Test 2', () => {
-        landingPage.clickClassAttribute_Page
+        landingPage.clickClassAttribute_Page()
     })
 
     it('Test 3', () => {
-        landingPage.clickHidenLayers_Page
+        landingPage.clickHidenLayers_Page()
     })
 
     it('Test 4', () => {
-        landingPage.clickLoadDelay_Page
+        landingPage.clickLoadDelay_Page()
     })
 
     it('Test 5', () => {
-        landingPage.clickAjaxData_Page
+        landingPage.clickAjaxData_Page()
     })
 
     it('Test 6', () => {
-        landingPage.clickClientSideDelay_Page
+        landingPage.clickClientSideDelay_Page()
     })
 
     it('Test 7', () => {
-        landingPage.clickClick_Page
+        landingPage.clickClick_Page()
     })
 
     it('Test 8', () => {
-        landingPage.clickDynamicTable_Page
+        landingPage.clickDynamicTable_Page()
     })
 
     it('Test 9', () => {
-        landingPage.clickTextInput_Page
+        landingPage.clickTextInput_Page()
     })
 
     it('Test 10', () => {
-        landingPage.clickScrollBars_Page
+        landingPage.clickScrollBars_Page()
     })
 
     it
     ('Test 11', () => {
-        landingPage.clickShadowDOM_Page
+        landingPage.clickShadowDOM_Page()
     })
 
     it
     ('Test 12', () => {
-        landingPage.clickVerifyText_Page
+        landingPage.clickVerifyText_Page()
     })
 
     it
     ('Test 13', () => {
-        landingPage.clickProgressBar_Page
+        landingPage.clickProgressBar_Page()
     })
 
     it
     ('Test 14', () => {
-        landingPage.clickVisibility_Page
+        landingPage.clickVisibility_Page()
     })
 
     it
     ('Test 15', () => {
-        landingPage.clickSampleApp_Page
+        landingPage.clickSampleApp_Page()
     })
 
     it
     ('Test 16', () => {
-        landingPage.clickMouseOver_Page
+        landingPage.clickMouseOver_Page()
     })
 
     it
     ('Test 17', () => {
-        landingPage.clickNonBreakingSpace_Page
+        landingPage.clickNonBreakingSpace_Page()
     })
 
     it
     ('Test 18', () => {
-        landingPage.clickOverlappedElement_Page
+        landingPage.clickOverlappedElement_Page()
     })
 })
