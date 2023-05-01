e37f3487f3c710151314885423effbd1713510a7
PeaceGusenga <gusengapeace@gmail.com>
2023-05-01 16:39:06 +0200

    add test case to assert button class and color

--- cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/classAttribute_Test.cy.js
+++ cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/classAttribute_Test.cy.js
@@ -4,7 +4,7 @@ import { IndexPage } from "../Pages/index_page"
 
 var landingpage = new IndexPage ()
 
-describe('Testing a site element that contains a Dynamic ID', () => {  
+describe('Testing class attributes', () => {  
    
     it ('Navigate to Class Attribute section', () => {
         cy.visit('http://www.uitestingplayground.com/')
@@ -13,6 +13,27 @@ describe('Testing a site element that contains a Dynamic ID', () => {
 
     it('Record Button Click', () => {
         cy.get('.class1').click()
-        
+
     })
+
+    it('find the button using btn-primary class', () => {
+        // use a CSS attribute selector to select the button with 
+        //the specific word in its class name
+        cy.get('button[class*=\'btn-primary\']') 
+        .should('have.css', 'background-color', 'rgb(0, 123, 255)') // assert the background color of the button
+/*
+In this example, my-class-name is the class name of the button you want to target. 
+The should() method allows you to make an assertion on the selected element. 
+The first argument to should() is the property you want to assert, which in this case 
+is the background-color property. The second argument is 
+the expected value of the property, which is rgb(0, 123, 255) in this case.
+
+Note that the value of the background-color property is returned 
+as an RGB string, so you need to compare it with the RGB value of the color you're 
+expecting. In this case, rgb(0, 123, 255) is the RGB value of the color #007BFF. 
+If the background color of the button is not #007BFF, 
+the assertion will fail and the test will report an error.
+*/
+    })
+
 })
