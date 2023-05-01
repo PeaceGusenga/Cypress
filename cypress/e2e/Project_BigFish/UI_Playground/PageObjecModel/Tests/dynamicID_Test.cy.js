49cd33b859680525cc61c3f149684cecf5094a62
PeaceGusenga <gusengapeace@gmail.com>
2023-05-01 15:44:54 +0200

    Dynamic Test Completed

--- cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/dynamicID_Test.cy.js
+++ cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/dynamicID_Test.cy.js
@@ -4,7 +4,7 @@ import { IndexPage } from "../Pages/index_page"
 
 var landingpage = new IndexPage ()
 
-describe('getting to the UI Playground Landing Page', () => {  
+describe('Testing a site element that contains a Dynamic ID', () => {  
    
     it ('Navigate to DynamicID section', () => {
         cy.visit('http://www.uitestingplayground.com/')
@@ -15,7 +15,7 @@ describe('getting to the UI Playground Landing Page', () => {
         cy.contains('Button with Dynamic ID').click()
     })
 
-    it('check button ID value', () => {
+    it('Assert button ID changes after button click', () => {
         //added static ID to assert ID changes
         cy.contains('Button with Dynamic ID').should('not.have.id','c12469b7-c98a-cae6-f0d6-29e4abff2b70')
     })
---
+++ cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/homepage_link_Test.cy.js
@@ -0,0 +1,3 @@
+import { IndexPage } from "../Pages/index_page";
+
+var landingPage = new IndexPage ()
