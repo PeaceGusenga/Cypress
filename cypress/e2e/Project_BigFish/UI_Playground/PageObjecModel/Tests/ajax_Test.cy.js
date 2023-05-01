0b92e37f6cf95ee7ea3794b477f4f507f735febf
PeaceGusenga <gusengapeace@gmail.com>
2023-05-01 18:01:01 +0200

    ajaxTest

---
+++ cypress/e2e/Project_BigFish/UI_Playground/PageObjecModel/Tests/ajax_Test.cy.js
@@ -0,0 +1,25 @@
+/// <reference types="Cypress" />
+
+import { IndexPage } from "../Pages/index_page"
+
+var landingpage = new IndexPage ()
+
+describe('Test to wait for an element to show up', () => {
+ 
+ 
+  it('should click a button, wait 15 seconds for ajax data', () => {
+    // Visit the page that contains the button
+    cy.visit('http://www.uitestingplayground.com/')
+    landingpage.clickAjaxData_Page()
+
+    // Find and click the button
+    cy.get('#ajaxButton').click();
+
+    // Wait for 15 seconds
+    cy.wait(15000);
+  })
+  it('verify new content loaded', () =>{
+   // Verify that new content has been loaded on the site
+   cy.get('#new-content').should('be.visible');
+  })
+});
